f460873e5a3807369983f92b2394f461d0add714
phuongnv63 <nguyenvanphuong06061994@gmail.com>
2024-11-08 03:33:10 +0700

    commit

---
+++ my-app/src/main/java/io/bootify/my_app/service/TaskService.java
@@ -0,0 +1,62 @@
+package io.bootify.my_app.service;
+
+import io.bootify.my_app.domain.Task;
+import io.bootify.my_app.model.TaskDTO;
+import io.bootify.my_app.repos.TaskRepository;
+import io.bootify.my_app.util.NotFoundException;
+import java.util.List;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(final TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<TaskDTO> findAll() {
+        final List<Task> tasks = taskRepository.findAll(Sort.by("id"));
+        return tasks.stream()
+                .map(task -> mapToDTO(task, new TaskDTO()))
+                .toList();
+    }
+
+    public TaskDTO get(final Long id) {
+        return taskRepository.findById(id)
+                .map(task -> mapToDTO(task, new TaskDTO()))
+                .orElseThrow(NotFoundException::new);
+    }
+
+    public Long create(final TaskDTO taskDTO) {
+        final Task task = new Task();
+        mapToEntity(taskDTO, task);
+        return taskRepository.save(task).getId();
+    }
+
+    public void update(final Long id, final TaskDTO taskDTO) {
+        final Task task = taskRepository.findById(id)
+                .orElseThrow(NotFoundException::new);
+        mapToEntity(taskDTO, task);
+        taskRepository.save(task);
+    }
+
+    public void delete(final Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    private TaskDTO mapToDTO(final Task task, final TaskDTO taskDTO) {
+        taskDTO.setId(task.getId());
+        taskDTO.setName(task.getName());
+        return taskDTO;
+    }
+
+    private Task mapToEntity(final TaskDTO taskDTO, final Task task) {
+        task.setName(taskDTO.getName());
+        return task;
+    }
+
+}
